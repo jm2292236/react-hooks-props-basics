143dd1e0fc9d0944b8f2dc0cc39c8202849be6b0
Jaime Montes de Oca <jaime0422@live.com>
2022-06-01 19:59:29 -0700

    Completed Assignment

--- src/components/BlogContent.js
+++ src/components/BlogContent.js
@@ -1,7 +1,19 @@
 import React from "react";
+import Comment from "./Comment";
 
 function BlogContent(props) {
-  return <div id="blog-content">{props.articleText}</div>;
+    if (!props.isPublished) {
+        return <h1>Unpublished blog! Try it later.</h1>;
+    } else {
+        return (
+            <div id="blog-content">
+                {props.articleText}
+                <Comment commentText="I agree with this statement. - Angela Merkel" />
+                <Comment commentText="A universal truth. - Noam Chomsky" />
+                <Comment commentText="Truth is singular. Its ‘versions’ are mistruths. - Sonmi-451" />
+            </div>
+        )
+    }
 }
 
 export default BlogContent;
